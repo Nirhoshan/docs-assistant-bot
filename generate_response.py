cf201f41676ea669cc02d28c75286fe9b21e02ec
Sivaroopan Nirhoshan <nirhosh98@gmail.com>
2024-09-25 13:22:42 +0530

    Add system prompt

--- generate_response.py
+++ generate_response.py
@@ -16,7 +16,7 @@ from langchain.schema import HumanMessage
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     llm, encodings, db, reranker, reranked_retriever = None, None, None, None, None
-    app.state.prompt = """You are a docbot that help users to understand more about Choreo Product usage by answering
+    app.state.system_prompt = """You are a docbot that help users to understand more about Choreo Product usage by answering
     their questions. Information from docs are given to to you help you answer the questions. If the information from docs
     are not relevant to your answer do not use it to construct your answer. If you don't have enough information to answer,
     refuse politely to answer the question. Do not hallucinate!
@@ -27,9 +27,8 @@ async def lifespan(app: FastAPI):
     In your answer always give the links of the most relevant doc from which you got the answer. You can use the
     doc_link metadata in the docs you are provided. Do not give fake links of your own. Do not always ask the user to
     refer the docs. Give a comprehensive answer to how to do it or what it is before you direct the user to the docs
-    with the links. Don't include steps to sign in to choreo console. User is already in Choreo console while asking you this question. 
-
-    User's Question: %s
+    with the links. Don't include steps to sign in to choreo console. User is already in Choreo console while asking you this question. """
+    app.state.user_prompt_template = """User's Question: %s
 
     Information from docs:
     """
@@ -84,10 +83,10 @@ async def get_docs(question):
 
 
 async def get_chat_prompt(question):
-    prompt, encodings = app.state.prompt, app.state.encodings
+    user_prompt_template, encodings = app.state.user_prompt_template, app.state.encodings
     cleaned_question = re.sub(r"in choreo", "", question, flags=re.IGNORECASE)
     results = await get_docs(cleaned_question)
-    chat_prompt = prompt % cleaned_question
+    chat_prompt = user_prompt_template % cleaned_question
     if len(results) == 0:
         chat_prompt += "No docs found for the question"
     chat_prompt_size = len(encodings.encode(chat_prompt))
@@ -104,10 +103,14 @@ async def get_chat_prompt(question):
 
 async def bulk_response(question):
     llm = app.state.llm
+    system_prompt = app.state.system_prompt
     message = await get_chat_prompt(question)
     # print(message)
     try:
-        response = await llm.ainvoke([HumanMessage(content=message)])
+        response = await llm.ainvoke([
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": message}
+        ])
         answer = response.content
     except Exception as e:
         logger.error(f"Error while generating llm response: {e}")
